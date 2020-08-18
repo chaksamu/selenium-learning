5edb8f4d629bb41767aafa70b0399560b3c12235
chaksamu <chaksmau@gmail.com>
2020-08-18 18:36:10 +0530

    sumoftwoNumbers

---
+++ src/sumoftwoNumbers.java
@@ -0,0 +1,12 @@
+
+public class sumoftwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 2;
+		int b = 2;
+		int sum = a + b;
+		System.out.println(sum);
+	}
+
+}
